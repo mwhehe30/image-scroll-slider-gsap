624de2ee2dd1d7f9076b7af0c722d6591daf89f5
Zakk <109673537+mwhehe30@users.noreply.github.com>
2025-09-11 07:25:17 +0700

    Add files via upload

---
+++ slides.js
@@ -0,0 +1,27 @@
+export const slides = [
+  {
+    tag: "Abstract Art",
+    marquee: "Cosmic Energy Fluid Composition",
+    image: "./img/img-1.jpg",
+  },
+  {
+    tag: "Data Art",
+    marquee: "67% Completion Visual Metaphor",
+    image: "./img/img-2.jpg",
+  },
+  {
+    tag: "Digital Painting",
+    marquee: "Nebula-Inspired Color Gradient",
+    image: "./img/img-3.jpg",
+  },
+  {
+    tag: "Light Art",
+    marquee: "Prismatic Reflection Study",
+    image: "./img/img-4.jpg",
+  },
+  {
+    tag: "Cyberpunk",
+    marquee: "Neon Grid Architecture",
+    image: "./img/img-5.jpg",
+  },
+];
